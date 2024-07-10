141016a3f589f8448335c6325eb96784f484f80d
Beuzz <yruama.peterschmitt@gmail.com>
2024-07-10 19:58:40 +0200

    update

--- via_beato/keymap.c
+++ via_beato/keymap.c
@@ -21,11 +21,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_A,    KC_Z,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,
+       KC_ESC,   KC_A,   KC_Z,   KC_E,   KC_R,   KC_T,                             KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TAB,    KC_Q,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_M, KC_QUOT,
+      KC_TAB,   KC_Q,   KC_S,   KC_D,   KC_F,   KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,   KC_M,   KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_W,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_COMM, KC_DOT,  KC_SCLN, KC_SLSH,  KC_LGUI,
+      KC_LSFT,   KC_W,   KC_X,   KC_C,   KC_V,   KC_B,                             KC_N, KC_COMM,  KC_DOT, KC_SCLN, KC_SLSH, KC_LGUI,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT, KC_LCTL,  KC_SPC,     KC_ENT, MO(1), MO(2)
                                       //`--------------------------'  `--------------------------'
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    return OLED_ROTATION_181;  // flips the display 180 degrees if offhand
   }
   return rotation;
 }
