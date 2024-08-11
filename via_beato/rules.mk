95a38d26953116c211d474889a97d43746e3e086
Georgi Koemdzhiev <koemdjiev@gmail.com>
2024-08-11 10:56:21 +0100

    Update rules.mk
    - Fix compilation error
    - Fix keyboard not recognised after a cold boot

--- via_beato/rules.mk
+++ via_beato/rules.mk
@@ -2,5 +2,6 @@ MOUSEKEY_ENABLE = no     # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 VIA_ENABLE      = yes    # Enable VIA
 OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
+OLED_DRIVER     = ssd1306
 LTO_ENABLE      = yes
+NO_USB_STARTUP_CHECK = yes # Fixes an issue where the kb won't be recognised after a cold boot
